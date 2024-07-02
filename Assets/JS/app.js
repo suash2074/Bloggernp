fb67db54e8f733a4cf29fc52399c307b1c18fcb2
suash <suash.rb@gmail.com>
2024-07-02 18:57:35 +0545

    BLOG DETAIL PAGE (Added heart button and counter in comment view)

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -33,3 +33,31 @@ function validateEmail() {
   }); // End Window Load
 })(jQuery, window, document);
 
+// $(document).ready(function () {
+//   $(".heart__btn").click(function () {
+//     $(".heart__btn").toggleClass("heart__active")
+//     $('.number').toggleClass("heart__active")
+//     $('.heart').toggleClass("heart__active")
+//   });
+// });
+
+$(document).ready(function () {
+  $(".heart__btn").click(function () {
+      let numberElement = $(this).find('.number');
+      let currentNumber = parseInt(numberElement.text());
+
+      // Check if currentNumber is a valid number
+      if (isNaN(currentNumber)) {
+          currentNumber = 0; // Default to 0 if parsing fails
+      }
+
+      if ($(this).hasClass("heart__active")) {
+          numberElement.text(currentNumber - 1);
+      } else {
+          numberElement.text(currentNumber + 1);
+      }
+
+      $(this).toggleClass("heart__active");
+      $(this).find('.heart').toggleClass("heart__active");
+  });
+});
