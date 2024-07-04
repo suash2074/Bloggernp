73ec5d21f82c54e7f36533069f151aa5c9e20093
suash <suash.rb@gmail.com>
2024-07-04 17:52:55 +0545

    SCROLL ANIMATION (About us ✅)

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -61,3 +61,18 @@ document.addEventListener('DOMContentLoaded', function () {
   console.log("Image URL:", imageUrl);
   contactUsSection.style.backgroundImage = imageUrl;
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
